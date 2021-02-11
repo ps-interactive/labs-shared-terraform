541338afbe759b16c161a9aed0db3638f0b125dc
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2021-02-11 10:24:50 -0700

    Create main.tf

---
+++ finished-labs/Azure/Build, Train, and Evaluate a Regression Model with Azure/main.tf
@@ -0,0 +1,76 @@
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+variable "location" {
+  default = "eastus"
+}
+# variable "tags" {
+# }
+
+data "azurerm_client_config" "current" {}
+
+resource "random_string" "random" {
+#   keepers = {
+#     # Generate a new id each time we switch a resource group
+#     rg = "var.resource_group_name"
+#   }
+  length  = 5
+  special = false
+  upper   = false
+  #   override_special = "/@£$"
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "pluralsight-regression-${random_string.random.result}"
+  location = var.location
+}
+
+#Creating Application Insights
+resource "azurerm_application_insights" "ps-ai-mlspace" {
+  name     = "ps-ai-mlspace"
+  location = var.location
+  #   resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+#Creating KeyVault
+resource "azurerm_key_vault" "ps-kv-mlspace" {
+  name     = "ps-kv-mlspace-${random_string.random.result}"
+  location = var.location
+  #   resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+#Creating Storage Account
+resource "azurerm_storage_account" "ps-strg-mlspace" {
+  name = "strgmlspace${random_string.random.result}"
+  #   resource_group_name      = var.resource_group_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#Creating Azure Machine Learning Workspace
+resource "azurerm_machine_learning_workspace" "ps-aml-mlspace" {
+  name     = "ps-aml-mlspace"
+  location = var.location
+  #   resource_group_name     = var.resource_group_name
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.ps-ai-mlspace.id
+  key_vault_id            = azurerm_key_vault.ps-kv-mlspace.id
+  storage_account_id      = azurerm_storage_account.ps-strg-mlspace.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# To reset random string
+# terraform taint random_string.random
+# terraform state rm random_string.random
