131580d9150cd459779f673d86b832d45d459a58
Stefan Nychka <stefannychka@gmail.com>
2021-09-13 14:10:12 -0600

    Make the app service, as per Tom's better workaround
    - local-exec failed at /labs, so still cannot deploy via tf (no AD, so
      can't use az cli non-interactively afaik)
    - Tom's at least allows the learners to use the portal, not the cli,
      to deploy the api

--- finished-labs/Azure/Manage APIs in Microsoft Azure with API Management/main.tf
+++ finished-labs/Azure/Manage APIs in Microsoft Azure with API Management/main.tf
@@ -7,21 +7,9 @@ variable "location" {
   default = "eastus"
 }
 resource "azurerm_resource_group" "rg" {
-  name = "pluralsight-resource-group"
+  name     = "pluralsight-resource-group"
   location = var.location
 }
-
-variable "remote_name" {
-  default = "production"
-}
-
-# variable "git_user" {
-#   default = "randomgitName"
-# }
-
-# variable "git_email" {
-#   default = "random@test.com"
-# }
 
 resource "random_id" "id" {
   keepers = {
@@ -43,4 +31,31 @@ resource "azurerm_storage_share" "lab_file_share" {
   name                 = "lab${random_id.id.hex}"
   storage_account_name = azurerm_storage_account.lab_storage_account.name
   quota                = 50
+}
+
+
+resource "azurerm_app_service_plan" "traffic_api_plan" {
+  name                = "traffic-api-plan-${random_id.id.hex}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "traffic_api_service" {
+  name                = "traffic-api-service-${random_id.id.hex}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.traffic_api_plan.id
+
+  site_config {
+    linux_fx_version          = "DOTNETCORE|3.1"
+    always_on                 = false
+    use_32_bit_worker_process = true
+  }
 }
